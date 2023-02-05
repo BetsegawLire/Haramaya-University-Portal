f562482c59f719fb9c7fad3a33016270d0b92457
BetsegawLire <betsegawhazard@gmail.com>
2023-02-05 04:45:22 -0800

    configure required modules

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+    projectId: 'student-portal-eed47',
+    appId: '1:258730352163:web:4f3a9d1dd9229920f49b96',
+    databaseURL: 'https://student-portal-eed47-default-rtdb.firebaseio.com',
+    storageBucket: 'student-portal-eed47.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBvLAyLS__mDzW-jp2PiEHUK-MgpGWiLNY',
+    authDomain: 'student-portal-eed47.firebaseapp.com',
+    messagingSenderId: '258730352163',
+  },
+    production: false,
+    
+};
